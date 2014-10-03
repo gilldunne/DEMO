ad0e33fc6d12e453113c66959d89a0dfb297265e
gilldunne <x00094469@ittd.ie>
2014-10-03 15:42:59 +0100

    Finished

--- Week3/script.sh
+++ Week3/script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # this is a comment
+#: Title : script.sh
+#: Date : 03/10/14
+#: Author : Gill Dunne
+#: Version : 1
+#: Description : Simple script to count cPU
+#: Options : 
 
 cpu=$( grep processor /proc/cpuinfo | wc -l )
 
