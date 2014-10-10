4b69ece9f31fa4998ec4818e375d25f9c08be35b
gilldunne <x00094469@ittd.ie>
2014-10-10 14:45:41 +0100

    Question 5

---
+++ Week4/Scripts/data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# this is a comment
+#: Title : data.sh
+#: Date : 10/10/14
+#: Author : Gill Dunne
+#: Version : 1
+#: Description : Data collection
+#: Options : 
+
+echo 'Hello '$1 ' you were born in ' $2 ' and your favourite color is ' $3
