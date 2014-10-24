cfa369917a70d257c7618bf0130487b419ef7737
gilldunne <x00094469@ittd.ie>
2014-10-24 15:40:45 +0100

    Week 6 diary

---
+++ Week6/diary.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# this is a comment
+#: Title : Diary.sh
+#: Date : 10/10/14
+#: Author : Gill Dunne
+#: Version : 1
+#: Description : Writing a diary
+#: Options : 
+read var
+
+echo $var >> Diary.txt
